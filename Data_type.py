8c3af375bf4b80960d68bbe7a51af01483902901
Sulimanalmoneef8 <almoneef.s@gmail.com>
2026-05-23 19:34:19 +0300

    inital commit

---
+++ Data_type.py
@@ -0,0 +1,14 @@
+#Display the data type
+
+a=8
+print("type of a:", type(a))
+
+b=5.5
+print("type of b", type(b))
+
+c="football"
+print("type of c", type(c))
+
+d=False
+print("type of d:", type(d))
+
