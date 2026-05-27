7b0918b727279f349b781a5be51e0154ccb264be
Sulimanalmoneef8 <almoneef.s@gmail.com>
2026-05-27 14:07:58 +0300

    initial commit

---
+++ Congratulations.py
@@ -0,0 +1,8 @@
+# User input
+word= input("Enter the word 'congratulations': " )
+
+# Converting to upper
+upper_word= word.upper()
+
+# Displaying the result
+print("Uppercase word is: ", upper_word)
